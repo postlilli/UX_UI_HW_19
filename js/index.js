fc035f21de3d158afae7673e9f90f4453884a2af
postlilli <113809630+postlilli@users.noreply.github.com>
2023-02-24 13:37:08 -0600

    Added Contact Me modal, UI interactions of Header& Footer list items/Card Hover states

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 console.log("Hello, World!");
+
+$(".headerItems").hover(function(){
+    $(this).css("font-weight", "700");
+},
+  function(){
+    $(this).css("font-weight", "400");
+});
+
+$(".workCard").hover(function(){
+    $(this).css("border", "5px solid black");
+},
+  function(){
+    $(this).css("border", "5px solid white");
+});
